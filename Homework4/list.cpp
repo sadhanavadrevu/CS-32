61d3975585fabc8ad8fca598c2205d9e77d16e72
sadhanavadrevu <sadhana.vadrevu@gmail.com>
2020-01-13 16:09:44 -0800

    Add files via upload

---
+++ Homework4/list.cpp
@@ -0,0 +1,16 @@
+//
+//  list.cpp
+//  hw4
+//
+
+void listAll(const MenuItem* m, string path)  // two-parameter overload
+{
+    if (m->menuItems() == NULL || (m->menuItems())->size() == 0)
+        return;
+    
+    for (vector<MenuItem*>::const_iterator it = m->menuItems()->begin(); it != m->menuItems()->end(); it++) {
+        cout << path + (*it)->name() << endl;
+        listAll((*it), path + (*it)->name() + "/");
+    }
+}
+
